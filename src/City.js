e134a4dd0df74b9d64e38a84edf3180851702cf2
Coni Hinojosa <coni@Conis-MacBook-Pro.local>
2023-08-25 16:08:10 +0100

    working version with balck and white text for contrast

--- src/City.js
+++ src/City.js
@@ -18,6 +18,13 @@ const weatherOptions = [
   "Débil ChubascosLluvia",
 ];
 
+// hex color to number
+// const colorNumber = parseInt("#3f5da2".replace("#", "0x"), 16)
+// complementary number
+// const complementNumber = 0xffffff ^ colorNumber;
+// hex number to hex color
+// const complementColor = `#${Number(complementNumber).toString(16)}`
+
 function City(props) {
   const [show, setShow] = useState(false);
   const [location, setLocation] = useState("");
@@ -27,13 +34,28 @@ function City(props) {
   const colors = props.colors;
   const cityJson = cities[city];
 
-  const getCurrentTimeOfDay = () => {
-    const chileTime = new Date().toLocaleString("en-US", {
-      timeZone: "America/Santiago",
-    });
-    const currentHour = new Date(chileTime).getHours();
-    return currentHour >= 9 && currentHour < 19 ? "morning" : "night";
+  // Define the function to get text color based on background color
+  const getContrastColor = (colors, index) => {
+    if (!colors || !colors[index]) {
+      console.log("Invalid color or index:", colors, index);
+      return "blanchedalmond"; // Default color if colors or selected index is undefined
+    }
+
+    const hexColor = colors[index];
+    const colorValue = parseInt(hexColor.replace("#", "0x"), 16);
+    console.log("Hex Color:", hexColor);
+    console.log("Color Value:", colorValue);
+
+    return colorValue > 0xffffff / 2 ? "#36454F" : "#F9F6EE";
   };
+
+  // const getCurrentTimeOfDay = () => {
+  //   const chileTime = new Date().toLocaleString("en-US", {
+  //     timeZone: "America/Santiago",
+  //   });
+  //   const currentHour = new Date(chileTime).getHours();
+  //   return currentHour >= 9 && currentHour < 19 ? "morning" : "night";
+  // };
 
   useEffect(() => {
     setShow(false); // Hide content
@@ -49,7 +71,6 @@ function City(props) {
       );
 
       setLocation(cityJson.location);
-
       setWeather(matchedWeather ? matchedWeather : "Despejado");
       // Show content after a delay
 
@@ -58,12 +79,13 @@ function City(props) {
   }, [city, cityJson.location]);
 
   return (
-    <div className={`${getCurrentTimeOfDay()}`}>
+    <div>
       <Card.Subtitle>
         <div
           className={`location-text ${
             show ? "fade-in opacity-visible" : "fade-out opacity-invisible"
           }`}
+          style={{ color: getContrastColor(colors, 0) }}
         >
           {location}
         </div>
@@ -73,10 +95,12 @@ function City(props) {
           className={`weather-text  ${
             show ? "fade-in opacity-visible" : "fade-out opacity-invisible"
           }`}
+          style={{ color: getContrastColor(colors, 1) }}
         >
           {weather}
         </div>
       </Card.Title>
+      <h5>{colors[0]}</h5>
     </div>
   );
 }
